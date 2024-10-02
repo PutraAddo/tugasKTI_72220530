0bb281a7aa88b397ee6a5ba0c4fafdbc69f567fd
PutraAddo <putra.addo19@gmail.com>
2024-10-03 00:12:07 +0700

    first comit

---
+++ SampleSecureWeb/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using SampleSecureWeb.Models;
+
+namespace SampleSecureWeb.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        
+        ViewBag.username = "Putra Addo Dhaya Abadi";
+
+        
+        List<string> fruits = new List<string> { "Banana", "Orange", "Apple" };
+
+        
+        ViewBag.fruits = fruits;
+
+        return View();
+    }
+
+    public IActionResult About()
+    {
+        ViewData["Title"] = "About page";
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
